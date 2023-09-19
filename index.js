2476585c3303e9002e48003cf1e5854217c1c9cc
Jorge Vidal Casa <jlvidal_2hotmail.com>
2023-09-18 19:46:54 -0500

    Se actuariza en require de mongoConnect para el error en render

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = require('./Server/routes/routes.js')
-const mongoConnect = require('../Ejemplo-Proyecto-final/Server/data/db.js')
+const mongoConnect = require('./Server/data/db.js')
 
 const app = express()
 const PORT = 3000
